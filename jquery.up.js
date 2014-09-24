23216ed4d808898045607a9c733387f19c535923
Finn-Lennart Heemeyer <finn@heemeyer.net>
2014-09-24 20:48:38 +0200

    Adds plugin

---
+++ jquery.up.js
@@ -0,0 +1,12 @@
+(function(window, document, $) {
+
+  $.fn.extend({
+    up: function(levels) {
+      for(var i = levels, self = this; i > 0; i--) {
+        self = self.parent();
+      }
+      return self;
+    }
+  });
+
+})(window, window.document, jQuery);
